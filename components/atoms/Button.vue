1973d39454462d6c6b18491763428ce13a910d7b
Yuri Araújo <yuri.hassle.araujo@gmail.com>
2021-07-20 02:53:30 -0400

    feat: add Button atom

---
+++ components/atoms/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button @click="$emit('handleButtonClick')">{{ label }}</button>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+
+  export default Vue.extend({
+    props: {
+      label: String,
+    },
+  });
+</script>
+
+<style scoped></style>
