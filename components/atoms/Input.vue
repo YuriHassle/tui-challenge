dc97170de1353d0636fcd50763d3c841b78567da
Yuri Araújo <yuri.hassle.araujo@gmail.com>
2021-07-22 06:52:38 -0400

    style: improve input style

---
+++ components/atoms/Input.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <input class="input" :placeholder="placeholder" />
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+
+  export default Vue.extend({
+    props: {
+      placeholder: String,
+    },
+  });
+</script>
+
+<style scoped lang="scss">
+  .input {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-end;
+    padding: 11px 12px 11px 8px;
+    width: 400px;
+    height: 48px;
+    background: #ffffff;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    box-sizing: border-box;
+    border-radius: 3px;
+    flex: none;
+    order: 1;
+    align-self: stretch;
+    flex-grow: 0;
+    margin: 24px 0px;
+    font-family: TUITypeLight LATN;
+    font-size: 20px;
+    line-height: 24px;
+    color: #092a5e;
+
+    &:focus {
+      border: 1px solid #10a0de;
+      box-shadow: 0px 0px 0px 4px #70cbf4;
+    }
+  }
+</style>
